f26ec0e5b7d1140153791480883b5c2c182a78eb
Dong Hee Jung <102508654+Jung-Dong-Hee@users.noreply.github.com>
2024-10-16 14:44:04 +0900

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from math import pi
 import pdfkit
+import re
 
 plt.style.use('ggplot')
 
@@ -19,21 +20,21 @@ except UnicodeDecodeError:
 df['Date'] = pd.to_datetime(df['Date'], format='%Y년 %m월', errors='coerce')
 
 translation_dict = {
-    '1차선': '1st',
-    '시리즈': 'Series',
-    '1차_1차선': '1st_1st',
-    '2차_1차선': '2nd_1st'
+    r'1차_1차선': '1st_1st',
+    r'2차_1차선': '2nd_1st',
+    r'1차선': '1st',
+    r'시리즈': 'Series'
 }
 
 def translate_ship_type(name):
     for key, value in translation_dict.items():
-        name = name.replace(key, value)
+        name = re.sub(key, value, name)
     return name
 
 df['선종'] = df['선종'].apply(translate_ship_type)
 
 st.sidebar.title("Filter Options")
-st.sidebar.header("필터링 옵션 설정")
+st.sidebar.header("분석 기간 및 대상 선종 선택")
 start_year = st.sidebar.selectbox("시작 연도 선택", [str(year) for year in range(2015, 2025)], index=8, key="start_year")
 start_month = st.sidebar.selectbox("시작 월 선택", [str(month).zfill(2) for month in range(1, 13)], index=6, key="start_month")
 end_year = st.sidebar.selectbox("종료 연도 선택", [str(year) for year in range(2015, 2025)], index=9, key="end_year")
@@ -101,26 +102,14 @@ else:
         st.write("**Normal**: 시운전 선박의 정상 운영을 의미합니다.")
         st.write("**Delayed**: 시운전 선박의 지연 운영을 의미합니다.")
 
-    def create_labor_cost_column(df):
-        df['인건비'] = df['선장비용'] + df['타수비용'] + df['노무원비용']
-        return df
-
-    def create_etc_cost_column(df):
-        df['기타비용'] = (df['양식'] + df['한식'] + df['용도품_침구'] + df['용도품_물품'] +
-                        df['예선료'] + df['통선비'])
-        return df
-
     def convert_to_millions(value):
         return value / 1_000_000
-
-    df_filtered = create_labor_cost_column(df_filtered)
-    df_filtered = create_etc_cost_column(df_filtered)
 
     statuses = ['정상', '지연', '전체']
     status_labels = {'전체': 'Total', '정상': 'Normal', '지연': 'Delayed'}
 
-    cost_types = ['유류비(\)', '인건비', '기타비용', '총 경비']
-    cost_type_labels = {'유류비(\)': 'Fuel Cost', '인건비': 'Labor Cost', '기타비용': 'Other Cost', '총 경비': 'Total Cost'}
+    cost_types = ['유류비(\)', '항해사비', '노무원비용', '기타비용', '총 경비']
+    cost_type_labels = {'유류비(\)': 'Fuel Cost', '항해사비': 'STN_Cost', '노무원비용': 'SMMT_Cost' , '기타비용': 'Other Cost', '총 경비': 'Total Cost'}
 
     data = {}
     for status in statuses:
@@ -148,13 +137,15 @@ else:
     plt.tight_layout()
 
     st.pyplot(fig)
-    with st.expander("비용 분석 결과 보기"):
+    
+    with st.expander("비용 분석 결과 보기 (in Millons)"):
         for status in statuses:
             st.write(f"**{status_labels[status]}**:")
             st.write(f"- Fuel Cost: {data[status][0]:.2f}")
-            st.write(f"- Labor Cost: {data[status][1]:.2f}")
-            st.write(f"- Other Cost: {data[status][2]:.2f}")
-            st.write(f"- Total Cost: {data[status][3]:.2f}")
+            st.write(f"- Sea Trials Navigator Cost: {data[status][1]:.2f}")
+            st.write(f"- Ship Maintenance and Management Team: {data[status][2]:.2f}")
+            st.write(f"- Other Cost: {data[status][3]:.2f}")
+            st.write(f"- Total Cost: {data[status][4]:.2f}")
 
     st.write("### 03. 정상/지연 항목별 비용 분석")
     st.write("선종의 정상 운영, 지연 운영, Total 운영의 총 경비, 인건비, 유류비를 시간의 흐름에 따른 분석")
@@ -165,7 +156,14 @@ else:
         st.write("**Total** : 시운전 선박의 모든 데이터를 의미합니다.")
         st.write("**Normal**: 시운전 선박의 정상 운영을 의미합니다.")
         st.write("**Delayed**: 시운전 선박의 지연 운영을 의미합니다.")
+        
+    def create_labor_cost_column(df):
+        df['인건비'] = df['선장비용'] + df['타수비용'] + df['노무원비용']
+        return df
 
+    # 비용 계산
+    df_filtered = create_labor_cost_column(df_filtered) 
+    
     df_normal = df_filtered[df_filtered['지연 여부'] == '정상']
     df_delay = df_filtered[df_filtered['지연 여부'] == '지연']
     df_total = df_filtered
@@ -212,14 +210,16 @@ else:
     st.write("### 04. 항목별 비용 특성 분석 (Radar Chart)")
     st.write("유류비, 인건비, 총 경비, 기타비용의 정상 운영과 지연 운영 간의 비율을 비교하여 분석")
     with st.expander("Radar Chart 설명 보기"):
-        st.write("**Fuel Cost**: 시운전 선박이 사용한 기름값을 나타냅니다.")
-        st.write("**STN_C**: Sea Trials Navigator Cost의 약자로 항해사비가 됩니다. 항해사비는 선장 비용, 타수 비용, 도선비, 임시항해 검사비, 자차 수정 비용 이 포함된 금액입니다.")
-        st.write("**SMMT_C**: Ship Maintenance and Management Team의 약자로 노무비가 됩니다.")
-        st.write("**Total Cost**: 시운전 선박의 총경비를 나타냅니다.")
-        st.write("**Other Cost** : 시운전 선박의 기타 경비를 나타냅니다. 기타 비용은 용도품 침구 및 물품, 예선료, 통선비, 양식, 한식 이 포함된 금액입니다.")
-        st.write("**Total** : 시운전 선박의 모든 데이터를 의미합니다.")
-        st.write("**Normal**: 시운전 선박의 정상 운영을 의미합니다.")
-        st.write("**Delay**: 시운전 선박의 지연 운영을 의미합니다.")
+        st.write("#### 항목명 : Radar plot 에서 각 꼭지점")
+        st.write("- Fuel Cost: 시운전 선박이 사용한 기름값을 나타냅니다.")
+        st.write("- STN_C: Sea Trials Navigator Cost의 약자로 항해사비가 됩니다. 항해사비는 선장 비용, 타수 비용, 도선비, 임시항해 검사비, 자차 수정 비용이 포함된 금액입니다.")
+        st.write("- SMMT_C: Ship Maintenance and Management Team의 약자로 노무비가 됩니다.")
+        st.write("- Other Cost: 시운전 선박의 기타 경비를 나타냅니다. 기타 비용은 용도품 침구 및 물품, 예선료, 통선비, 양식, 한식이 포함된 금액입니다.")
+        st.write("- Total Cost: 시운전 선박의 총경비를 나타냅니다.")
+        st.write("#### 운영 결과")
+        st.write("- **Normal**: 시운전 선박의 정상 운영을 의미합니다.")
+        st.write("- **Delay**: 시운전 선박의 지연 운영을 의미합니다.")
+        st.write("- **Total**: 시운전 선박의 모든 데이터를 의미합니다.")
 
     def calculate_cost_ratios(df):
         cost_columns = ['유류비(\)', '항해사비', '노무원비용', '총 경비', '기타비용']
@@ -238,12 +238,7 @@ else:
                 'Delay': (delayed_avg['유류비(\)'] / total_avg['유류비(\)']) * 110,
                 'Total': 100
             },
-            'STN_C': {
-                'Normal': (normal_avg['항해사비'] / total_avg['항해사비']) * 90,
-                'Delay': (delayed_avg['항해사비'] / total_avg['항해사비']) * 110,
-                'Total': 100
-            },
-            'SMMT_C': {
+	        'SMMT_C': {
                 'Normal': (normal_avg['노무원비용'] / total_avg['노무원비용']) * 90,
                 'Delay': (delayed_avg['노무원비용'] / total_avg['노무원비용']) * 110,
                 'Total': 100
@@ -257,13 +252,18 @@ else:
                 'Normal': (normal_avg['기타비용'] / total_avg['기타비용']) * 90,
                 'Delay': (delayed_avg['기타비용'] / total_avg['기타비용']) * 110,
                 'Total': 100
+            },
+            'STN_C': {
+                'Normal': (normal_avg['항해사비'] / total_avg['항해사비']) * 90,
+                'Delay': (delayed_avg['항해사비'] / total_avg['항해사비']) * 110,
+                'Total': 100
             }
         }
 
         return ratios
 
     def plot_radar_chart(ratios):
-        categories = ['Fuel Cost', 'STN_C', 'SMMT_C', 'Total Cost', 'Other Cost']
+        categories = ['Fuel Cost', 'SMMT_C', 'Total Cost', 'Other Cost', 'STN_C']
 
         normal_values = [ratios[cat]['Normal'] for cat in categories]
         delayed_values = [ratios[cat]['Delay'] for cat in categories]
@@ -294,7 +294,7 @@ else:
         ax.plot(angles, delayed_values, linewidth=1, linestyle='solid', label='Delay', color='#ffc81b')
         ax.fill(angles, delayed_values, alpha=0.1, color='#ffc81b')
 
-        plt.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
+        plt.legend(loc='upper right', bbox_to_anchor=(0.05, 0.05))
         plt.title('Radar chart by cost')
 
         st.pyplot(fig)
@@ -312,7 +312,7 @@ else:
                 'enable-local-file-access': None
             }
 
-            url = st.get_option('browser.gathered_url')
+            url = st.get_option('https://sea-trails.streamlit.app/')
             pdfkit.from_url(url, 'output.pdf', options=pdf_options)
 
             st.success("PDF 파일이 성공적으로 저장되었습니다. output.pdf")
