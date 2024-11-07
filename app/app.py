d3c044da8edbbd02819ab0273841621e1ffd1adf
Dong Hee Jung <102508654+Jung-Dong-Hee@users.noreply.github.com>
2024-11-07 10:34:30 +0900

    Update app.py

--- app/app.py
+++ app/app.py
@@ -10,9 +10,9 @@ from matplotlib.collections import LineCollection
 
 st.set_page_config(page_title="Sea Trials - Streamlit", layout="centered")
 
-primaryColor = "#FF4B4B"            
-backgroundColor = "#ebf1f1"          
-secondaryBackgroundColor = "#0d6efd" 
+primaryColor = "#494949"            
+backgroundColor = "#0d6efd"          
+secondaryBackgroundColor = "#ebf1f1" 
 textColor = "#494949"                
 
 custom_css = f"""
